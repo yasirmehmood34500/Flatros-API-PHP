30cfa1d13af871823fbb1ce5cb21f02da0ab1bb7
Yasir Memood <yasirmehmood34500@gmail.com>
2021-01-29 16:24:46 +0500

    conne

--- classes/dbClass.php
+++ classes/dbClass.php
@@ -1,1 +1,1 @@
- <?php class dbClass {protected $conn; function __construct() {$this->conn=new mysqli("localhost", "root","","flatros"); } } $obj =new dbClass(); ?>
+ <?php class dbClass {protected $conn; function __construct() {$this->conn=new mysqli("mysql-19382-0.cloudclusters.net","flatros_api_php_user","@Flatros12345","flatros_api_php"); } } $obj =new dbClass(); ?>
